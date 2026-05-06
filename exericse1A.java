8ebf96c24c55cd6d5151c4a37536006a8f30998d
Stefanos <srb00@aubmed.ac.cy>
2026-05-06 17:18:35 +0300

    first commit

---
+++ exericse1A.java
@@ -0,0 +1,11 @@
+public class exericse1A {
+    public static void main(String[] args) {
+        int score = 90;
+        double gpa= 4.0;
+        boolean isEnrolled= true;
+
+        System.out.println(score);
+        System.out.print(gpa);
+        System.out.print(isEnrolled);
+    }
+}
